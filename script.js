56550e9e0a935a244fa2b48f26b6241ae1f0863c
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2024-09-24 11:11:47 +0500

    oxirgi variant

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 var set = cartObj();
 var matnBuyurtma = "Sizning buyurtmangiz: ";
-var narx=0
+var narx = 0;
+var bb = set;
 for (const zakaz in set) {
-  const buyurtma = set[zakaz];
-  var bb=zakaz
-  
-  for (const b in buyurtma) {
-    matnBuyurtma.concat(bb).concat(" ").concat(buyurtma[b].info)
-    narx+=buyurtma[b].price
-  }
-  
-  matnBuyurtma.concat(narx)
-  console.log(matnBuyurtma)
+  matnBuyurtma=matnBuyurtma.concat(zakaz)
+  matnBuyurtma=matnBuyurtma.concat(" (")
+  matnBuyurtma=matnBuyurtma.concat(set[zakaz].info)
+  matnBuyurtma=matnBuyurtma.concat("), ")
+  narx += set[zakaz].price;
+    
 }
-
+matnBuyurtma=matnBuyurtma.concat("Etkazib berish xizmati bilan narxi "+(narx+9000)+" sum. Etkazib berish xizmati 9000 sum");
+console.log(matnBuyurtma);
